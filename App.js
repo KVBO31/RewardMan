f88de9213ef652cf9ab9d6129f821f96036b9813
voronov-nikita <voronovnr_1@mail.ru>
2024-11-25 17:07:57 +0300

    add navigator

--- App.js
+++ App.js
@@ -6,28 +6,45 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
-import { StyleSheet, Text, View } from "react-native";
+import { SettingsScreen } from "./src/pages/SettingsPage";
+import { ProfileScreen } from "./src/pages/ProfilePage";
+import { HomeScreen } from "./src/pages/HomePage";
+import { AuthScreen } from "./src/pages/AuthPage";
 
 export default function App() {
-  return (
-    // <Test />
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Auth"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#ffffff",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="Auth"
-          component={AuthScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Auth"
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: "#ffffff",
+					},
+				}}
+			>
+				<Stack.Screen
+					name="Auth"
+					component={AuthScreen}
+					options={{
+						headerShown: false,
+					}}
+				/>
+
+				<Stack.Screen
+					name="Home"
+					component={HomeScreen}
+				/>
+
+				<Stack.Screen
+					name="Profile"
+					component={ProfileScreen}
+				/>
+
+				<Stack.Screen
+					name="Settings"
+					component={SettingsScreen}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
---
+++ src/pages/AuthPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+
+
+export const AuthScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Ужас</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+	container: {
+	},
+});
